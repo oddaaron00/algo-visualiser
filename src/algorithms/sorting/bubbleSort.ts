ede8e09b6b8d8a8db2e31aa424433cb4cb57bddb
oddaaron00 <caseygoodings@outlook.com>
2021-03-15 16:25:31 +0000

    a

--- src/algorithms/sorting/bubbleSort.ts
+++ src/algorithms/sorting/bubbleSort.ts
@@ -1,23 +1,27 @@
 export function bubbleSort(array: number[], setGridArr: (newGridArr: number[]) => void) {
-    let n: number = array.length;
-    let newn: number;
+    var state = {
+        array: array,
+        setGridArr: setGridArr,
+        n: array.length,
+        newn: 0
+    }
     do {
-        newn = 0;
-        for (let i = 1; i < n; i++) {
-            if (array[i - 1] > array[i]) {
-                [array[i - 1], array[i]] = [array[i], array[i - 1]];
-                setGridArr([...array]);
-                newn = i;
-            }
+        state.newn = 0;
+        for (let i = 1; i < state.n; i++) {
+            pass(state);
         }
-        n = newn;
-    } while (n > 1);
+        state.n = state.newn;
+    } while (state.n > 1);
 }
 
-function pass() {
-
+function pass(state: { array: number[], setGridArr: (newGridArr: number[]) => void, n: number, newn: number }) {
+    console.log("PASS");
 }
 
-function step() {
-    
+function step(state: { array: number[], setGridArr: (newGridArr: number[]) => void, n: number, newn: number, i: number }) {
+    if (state.array[state.i - 1] > state.array[state.i]) {
+        [state.array[state.i - 1], state.array[state.i]] = [state.array[state.i], state.array[state.i - 1]];
+        state.setGridArr([...state.array]);
+        state.newn = state.i;
+    }
 }
