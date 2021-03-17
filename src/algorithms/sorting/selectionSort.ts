a736392eac06f0275eb442e64136e36092b7954e
oddaaron00 <caseygoodings@outlook.com>
2021-03-17 09:41:12 +0000

    Update selectionSort.ts

--- src/algorithms/sorting/selectionSort.ts
+++ src/algorithms/sorting/selectionSort.ts
@@ -1,82 +1,5 @@
 import { State } from "../../interfaces/ICell";
 
-async function sortPrev(array: number[], setArray: (newArray: number[]) => void, stateArr: State[], setStateArr: (newStateArr: State[]) => void) {
-    const step = (jMin: number, j: number) => {
-        console.log(jMin, j)
-        if (array[j] < array[jMin]) {
-            jMin = j;
-        }
-    }
-
-    const pass = (i: number, j: number) => {
-        console.log('j', j)
-        let jMin = i;
-        //for (j = i + 1; j < aLength; j++) {
-        //    step(jMin, j);
-        //}
-        return new Promise((resolve: (value: [jMin: number, i: number]) => any) => {
-            console.log(i, j)
-            const stepInterval = setInterval(() => {
-                if (j !== jMin) {
-                    stateArr[j] = State.Filled;
-                    setStateArr([...stateArr]);
-                } else if (j === i) {
-                    console.log("a")
-                    stateArr[j] = State.CurrentMin;
-                    setStateArr([...stateArr]);
-                }
-                j++;
-                stateArr[j] = State.Current;
-                setStateArr([...stateArr]);
-                if (array[j] < array[jMin]) {
-                    console.log('b')
-                    stateArr[jMin] = State.Filled;
-                    setStateArr([...stateArr]);
-                    jMin = j;
-                    stateArr[jMin] = State.CurrentMin;
-                    setStateArr([...stateArr]);
-                }
-
-                if (j > aLength) {
-                    stateArr[jMin] = State.Filled;
-                    setStateArr([...stateArr]);
-                    resolve([jMin, i]);
-                    clearInterval(stepInterval);
-                }
-            }, 200)
-        });
-    }
-
-
-    let i: number;
-    let j: number;
-    let aLength = array.length;
-
-    for (i = 0; i < aLength - 1; i++) {
-        j = i;
-        await pass(i, j).then((vals: [jMin: number, i: number]) => {
-            let [jMin, i] = vals;
-            if (jMin !== i) {
-                [array[i], array[jMin]] = [array[jMin], array[i]];
-            }
-            stateArr[i] = State.Sorted;
-            setStateArr([...stateArr]);
-            setArray([...array]);
-            console.log('async')
-        })
-            //if (jMin !== i) {
-            //    [array[i], array[jMin]] = [array[jMin], array[i]];
-            //}
-            //stateArr[i] = State.Sorted;
-            //setStateArr([...stateArr]);
-            //setArray([...array]);
-    }
-    stateArr[aLength - 1] = State.Sorted;
-    setStateArr([...stateArr]);
-
-
-}
-
 export default async function sort(array: number[], setArray: (newArray: number[]) => void, stateArr: State[], setStateArr: (newStateArr: State[]) => void) {
 
     const step = (i: number) => {
@@ -106,7 +29,7 @@ export default async function sort(array: number[], setArray: (newArray: number[
                     resolve([jMin, i]);
                     clearInterval(stepInterval);
                 }
-            }, 50)
+            }, 250)
         });
     }
 
