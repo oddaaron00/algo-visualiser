3af0a1b8b0a98f2311eb8bb28dd05cd70a085f67
oddaaron00 <caseygoodings@outlook.com>
2021-03-18 09:06:27 +0000

    Changed menu styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,18 +5,28 @@ import * as i from '../interfaces/IMenu';
 export default function Menu({ sortingAlgs, currentAlg, setAlg, resetGrid, randomiseData, visualise, speed, setSpeed }: i.Menu) {
 
     return (
-        <header id='menu' className='pure-form'>
-            <fieldset>
-                <label htmlFor='sortingAlgorithms'>Algorithm: 
-                <select id='sortingAlgorithms' value={currentAlg} onChange={e => setAlg(e.target.value)}>
-                    {sortingAlgs.map(alg => <option key={alg} value={alg}>{alg}</option>)}
-                </select>
-                </label>
-                <input type='number' id='speed' name='speed' value={speed} onChange={e => setSpeed(Number(e.target.value))}></input>
-                <button id='visualiseButton' className='pure-button pure-button-primary' onClick={visualise}>Visualise</button>
-                <button id='randomiseButton' className='pure-button' onClick={randomiseData}>Randomise data</button>
-                <button id='resetButton' className='pure-button' onClick={resetGrid}>Reset grid</button>
-            </fieldset>
+        <header id='menu'>
+            <form className='pure-form'>
+                <fieldset>
+                    <div className='outerContainer'>
+                        <div className='selectGroup'>
+                            <label className='selectLabel' htmlFor='sortingAlgorithms'>Algorithm</label>
+                            <select id='sortingAlgorithms' value={currentAlg} onChange={e => setAlg(e.target.value)}>
+                                {sortingAlgs.map(alg => <option key={alg} value={alg}>{alg}</option>)}
+                            </select>
+                        </div>
+                        <div className='selectGroup'>
+                            <label className='selectLabel' htmlFor='speed'>Speed per step (milliseconds)</label>
+                            <input type='number' id='speed' name='speed' value={speed} onChange={e => setSpeed(Number(e.target.value))}></input>
+                        </div>
+                    </div>
+                    <div className='pure-button-group' role='group'>
+                        <button id='randomiseButton' className='pure-button' onClick={e => {e.preventDefault(); randomiseData()}}>Randomise data</button>
+                        <button id='visualiseButton' type='submit' className='pure-button pure-button-primary' onClick={e => {e.preventDefault(); visualise()}}>Visualise</button>
+                        <button id='resetButton' className='pure-button' onClick={e => {e.preventDefault(); resetGrid()}}>Reset grid</button>
+                    </div>
+                </fieldset>
+            </form>
         </header>
     )
 }
